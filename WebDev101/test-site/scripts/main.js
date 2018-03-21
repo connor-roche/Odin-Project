5c050ecbb950b98d23b8bfa2cdd0c5f77a5f02b4
Connor Roche <connor.roche@gmail.com>
2018-03-20 21:07:57 -0400

    init commit

---
+++ WebDev101/test-site/scripts/main.js
@@ -0,0 +1,32 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Polarlicht_2.jpg') {
+	myImage.setAttribute('src', 'images/image2.jpg');
+    } else {
+	myImage.setAttribute('src','images/Polarlicht_2.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Hi' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hi' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
